65c18612e71352194a87219660670a8da2df0aee
Jake Meyer <84546278+Jake-OMES@users.noreply.github.com>
2024-05-05 12:05:09 -0500

    Added Llama-3

--- constants.py
+++ constants.py
@@ -97,11 +97,15 @@ EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"  # Uses 1.5 GB of VRAM (High Ac
 #### (FOR GGUF MODELS)
 ####
 
+# LLAMA 3 # use for NVIDIA GPUs
+MODEL_ID = "unsloth/llama-3-8b-bnb-4bit"
+MODEL_BASENAME = None
+
 # MODEL_ID = "TheBloke/Llama-2-13b-Chat-GGUF"
 # MODEL_BASENAME = "llama-2-13b-chat.Q4_K_M.gguf"
 
-MODEL_ID = "TheBloke/Llama-2-7b-Chat-GGUF"
-MODEL_BASENAME = "llama-2-7b-chat.Q4_K_M.gguf"
+# MODEL_ID = "TheBloke/Llama-2-7b-Chat-GGUF"
+# MODEL_BASENAME = "llama-2-7b-chat.Q4_K_M.gguf"
 
 # MODEL_ID = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
 # MODEL_BASENAME = "mistral-7b-instruct-v0.1.Q8_0.gguf"
